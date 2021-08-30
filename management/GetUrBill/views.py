ccd91a7ec148afe55ebcbcbe8f4fc7ed1ff4d483
Gourav5015 <aggourav5015@gmail.com>
2021-08-30 13:36:02 +0530

    Loader

--- management/GetUrBill/views.py
+++ management/GetUrBill/views.py
@@ -18,17 +18,15 @@ def home(request):
 @login_required
 def dashboard(request):
     if request.method=="POST":
-        c=request.POST['count']
-        c=int(c)
-        for i in range(0,c+1):
-            item_name=request.POST['item_name'+str(i)]
-            quantity=request.POST['quantity'+str(i)]
-            discount=request.POST['discount'+str(i)]
-            gst=request.POST['gst'+str(i)]
-            price=request.POST['price'+str(i)]
-            user=request.user
-            item=Items(user=user,item_name=item_name,quantity=quantity,GST=gst,discount=discount,selling_price=price)
-            item.save()
+
+        item_name=request.POST['item_name']
+        quantity=request.POST['quantity']
+        discount=request.POST['discount']
+        gst=request.POST['gst']
+        price=request.POST['price']
+        user=request.user
+        item=Items(user=user,item_name=item_name,quantity=quantity,GST=gst,discount=discount,selling_price=price)
+        item.save()
         return redirect("/dashboard/")
     items=Items.objects.all().filter(user=request.user)
     context={"i":items}
