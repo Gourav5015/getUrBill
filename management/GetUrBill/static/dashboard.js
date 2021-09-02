381d38cc155f74deb001d4095817d1cebdeb1f29
YuktiButolia <yuktibutolia@gmail.com>
2021-09-02 13:17:18 +0530

    form validation

--- management/GetUrBill/static/dashboard.js
+++ management/GetUrBill/static/dashboard.js
@@ -5,3 +5,15 @@ function logout(){
 function load(){
    document.getElementById("load-img").style.display="flex";
 };
+document.getElementById("add").addEventListener("click",function(){
+   item=document.forms["form"]["item_name"].value
+   quantity=document.forms["form"]["quantity"].value
+   price=document.forms["form"]["price"].value
+   gst=document.forms["form"]["gst"].value
+   discount=document.forms["form"]["discount"].value
+   if(item!=='' && quantity!=='' && price!=='' && discount!=='' && gst!=='')
+   {
+      load()
+   }
+
+})
