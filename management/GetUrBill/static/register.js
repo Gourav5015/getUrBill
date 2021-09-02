744175674e22a9f1351ac8ce02452b75ce2ca166
DipeshMht <83875858+DipeshMht@users.noreply.github.com>
2021-09-02 11:11:02 +0530

    Add files via upload

--- management/GetUrBill/static/register.js
+++ management/GetUrBill/static/register.js
@@ -7,3 +7,23 @@ next.addEventListener("click",function(){
     document.getElementById("staffpass").style.display="block"
 });
 
+phone=document.getElementById("phonenumber");
+phone.addEventListener("change",function(){
+    phone=phone.value;
+    $.ajax({
+        type:"GET",
+        url:`/check/${phone}/`,
+        success: function(status){
+            if(status.status=="OK")
+            {
+            $("#msg").text(status.status)
+            $("#msg").attr("class","alert-success")
+            $("#msg").show()
+            }
+            else{
+                $("#msg").text(status.status)
+                $("#msg").show()
+            }
+        }
+    });
+});
